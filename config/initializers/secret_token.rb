0402082529594628dc5736687787ac0349089ed1
Olly Barrett <oliver.barrett@chaseresponse.com>
2012-06-28 19:26:59 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CSAPP::Application.config.secret_token = '2c950c27910bd5dc99309dc2afeb35e647328b94a607263295da25244800f7a0fd7e9e31127ea130f0b25574fbf679c1daf647dc58d87b767a10076d5e0811d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CSAPP::Application.config.session_store :cookie_store, key: '_CSAPP_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CSAPP::Application.config.session_store :active_record_store
